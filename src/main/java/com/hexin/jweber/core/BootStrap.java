15339b0e5c2c0a8b71e04471b43d1b2d29eb5378
justin201508 <toto_66@163.com>
2015-09-02 12:51:42 +0800

    create project base structure

---
+++ src/main/java/com/hexin/jweber/core/BootStrap.java
@@ -0,0 +1,18 @@
+package com.hexin.jweber.core;
+
+/**
+ * BootStrap for jweber java web server
+ * 
+ * first ,do some server initilzation works:
+ * - this class will construct a classloader for use in load server internal classes.
+ * - set some default environments 
+ * - 
+ * 
+ * 
+ * 
+ * @author suntao@myhexin.com
+ *
+ */
+public class BootStrap {
+
+}

991786449bf80752cd0391f9df8295679e7a85e1
justin201508 <toto_66@163.com>
2015-09-02 15:38:27 +0800

    //startup package

---
+++ src/main/java/com/hexin/jweber/core/startup/Catalina.java
@@ -0,0 +1,23 @@
+package com.hexin.jweber.core.startup;
+
+public class Catalina {
+	
+	private ClassLoader parentClassloader = null;
+
+	
+	
+	public ClassLoader getParentClassloader() {
+		return parentClassloader;
+	}
+
+	public void setParentClassloader(ClassLoader parentClassloader) {
+		this.parentClassloader = parentClassloader;
+	}
+	
+	
+	public void start() {
+		
+	}
+	
+
+}
